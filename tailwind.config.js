433cdf33bcc396e34458a279f26a0ed1b49ca08f
Abdur-P-Rahim <bspirahim@gmail.com>
2024-01-19 11:27:03 +0600

    header added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,21 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#ff8b42",
+          "secondary": "#6e27e0",
+          "accent": "#0089ba",
+          "neutral": "#273240",
+          "base-100": "#ffffff",
+        },
+      },
+    
+    ],
   },
+
   plugins: [require("daisyui")],
 }
 
